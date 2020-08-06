979427e57ee7f2b5c7870e72e377a79e0dd51f70
Victoria <victoria.eisemann@gmail.com>
2020-08-06 10:23:12 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,35 @@
+function add(a,b) {
+  return (a+b);
+}
+
+function subtract(a,b) {
+  return (a-b);
+}
+
+function multiply(a,b) {
+  return (a*b);
+}
+
+function divide(a,b) {
+  return (a/b);
+}
+
+function increment(n) {
+  n++;
+  return n;
+}
+
+function decrement(n) {
+  n--;
+  return n;
+}
+
+function makeInt(n){
+  parseInt(2, 10);
+  return n;
+}
+
+function preserveDecimal(n){
+  parseFloat(2.2222);
+  return n;
+}
